d1070cf86e3729586eaa708e10d2ec7e006f74bd
newsoft <newsoft@newsoftdeiMac.local>
2018-01-05 14:37:21 +0800

    ddd

---
+++ TestCode/TestCode/AppDelegate.m
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.m
+//  TestCode
+//
+//  Created by newsoft on 05/01/2018.
+//  Copyright © 2018 newsoft. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+    
+    NSLog(@"hello");
+    
+}
+
+
+@end

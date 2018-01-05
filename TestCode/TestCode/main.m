1e658dd93be85376b0363f00b2284aba4c391762
newsoft <newsoft@newsoftdeiMac.local>
2018-01-05 14:38:11 +0800

    new

---
+++ TestCode/TestCode/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  TestCode
+//
+//  Created by newsoft on 05/01/2018.
+//  Copyright © 2018 newsoft. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}

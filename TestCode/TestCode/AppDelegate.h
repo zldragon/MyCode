5387bc3e66464b4962f2d952a3458941c9e54900
newsoft <newsoft@newsoftdeiMac.local>
2018-01-05 14:37:35 +0800

    dd

---
+++ TestCode/TestCode/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestCode
+//
+//  Created by newsoft on 05/01/2018.
+//  Copyright © 2018 newsoft. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+

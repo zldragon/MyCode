4898c33e4b18948e8ebc731d443d08c4adb47ae9
newsoft <newsoft@newsoftdeiMac.local>
2018-01-05 14:37:50 +0800

    new

---
+++ TestCode/TestCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestCode
+//
+//  Created by newsoft on 05/01/2018.
+//  Copyright © 2018 newsoft. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
